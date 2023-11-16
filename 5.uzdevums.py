9de1b0ce211cc495c17ad248a68f61d14c231f4e
unknown <zane.laure@grobinasskola.lv>
2023-11-16 14:52:10 +0200

    Pievienots 5.uzdevums.py

---
+++ 5.uzdevums.py
@@ -0,0 +1,2 @@
+#Izveidot Python programmu, kas atkarībā no pašreizējās stundas izvada atbilstošu sveicienu, izmantojot if priekšrakstu. (labrīt, labdien, labvakar)
+#Izmantot GIT, lai izsekotu kodu un veidotu komitus.
