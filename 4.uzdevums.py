9ca1baf652b496d6ed2bc4fdc99582321602f9f5
unknown <zane.laure@grobinasskola.lv>
2023-11-16 15:23:22 +0200

    Init commit

--- 4.uzdevums.py
+++ 4.uzdevums.py
@@ -1,2 +1,8 @@
 #IzveidoT Python programmu, kas aprēķina lietotāja ievadīta skaitļa faktoriālu, izmantojot for ciklu.
 #Izmantot GIT, lai izsekotu kodu un veidotu komitus.
+def faktiorialis(x):
+    rezultats = 1
+for i in range(1, x+1):
+    rezultats *=i
+    return rezultats
+print(faktiorialis(x))
