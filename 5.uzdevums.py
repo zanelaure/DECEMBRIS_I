37ae9d5e350d58b7802b2ee161d126cf4c96d2b7
unknown <zane.laure@grobinasskola.lv>
2023-11-16 14:57:39 +0200

    Init commit

--- 5.uzdevums.py
+++ 5.uzdevums.py
@@ -1,2 +1,3 @@
 #Izveidot Python programmu, kas atkarībā no pašreizējās stundas izvada atbilstošu sveicienu, izmantojot if priekšrakstu. (labrīt, labdien, labvakar)
 #Izmantot GIT, lai izsekotu kodu un veidotu komitus.
+if __name__ == '__main__': print('Labdien!')
